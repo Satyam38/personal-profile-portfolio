a7fde24adf66e191ed6993bbe0bf42a9f8fd75d7
Coder Sathu <89405234+Satyam868910@users.noreply.github.com>
2022-07-16 10:33:19 +0530

    Add files via upload

---
+++ portfolia/script.js
@@ -0,0 +1,8 @@
+//            typing
+
+var typed = new Typed(".typing",{
+    strings:["", "Student (AITH) ", "Web Development ", "Youtuber"],
+    typeSpeed:100,
+    BackSpeed: 60,
+    loop:true
+})
